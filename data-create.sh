0c1a79332aae8ba31df5d32807415317dbe2f7c5
limelime <limelime@users.noreply.github.com>
2017-04-27 13:13:30 -0400

    Add create data script.

---
+++ data-create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Description: Create test data.
+
+test_data_dir='test-data'
+td_level2_dir="${test_data_dir}/lvl2"
+td_level3_dir="${test_data_dir}/lvl3"
+mkdir -p "${test_data_dir}"
+mkdir -p "${td_level2_dir}"
+mkdir -p "${td_level3_dir}"
+
+touch "${test_data_dir}/file_root.pdf"
+touch "${test_data_dir}/file_root.txt"
+
+touch "${td_level2_dir}/file_lvl2.pdf"
+touch "${td_level2_dir}/file_lvl2.txt"
+
+touch "${td_level3_dir}/file_lvl3.pdf"
+touch "${td_level3_dir}/file_lvl3.txt"
